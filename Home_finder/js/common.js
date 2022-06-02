ae6b311a782c1eb3a4c6627f4527cecf3abbc9ef
hytechpro-payalbiyani <payal.biyani@hytechpro.com>
2022-06-02 23:25:51 +0530

    property search mobile and desktop view

--- Home_finder/js/common.js
+++ Home_finder/js/common.js
@@ -429,34 +429,4 @@ function closeNav() {
   document.getElementsByClassName("owl-carousel")[4].style.zIndex = "1";
 }
 
-// search for property grid listing view JS
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
-
-// Declare a loop variable
-var i;
-
-// List View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
-}
-
-/* Optional: Add active class to the current button (highlight it) */
-var container = document.getElementById("btnContainer");
-var btns = container.getElementsByClassName("btn1");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// property search script
